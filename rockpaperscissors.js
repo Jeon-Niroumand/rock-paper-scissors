b9b7578c2addec6022767fa589ec54266b468cfc
saxbonsai <jeon.niroumand@gmail.com>
2022-07-08 00:46:36 -0700

    included variables as part of message playRound()

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -2,21 +2,21 @@ const playerSelection = prompt("Type: Rock, Paper or Scissors to play and press 
 console.log(`You chose ${playerSelection.toLowerCase()}`);
 
 var items = ['rock','paper','scissors'];
-var computerSelection = computerPlay(items);
+const computerSelection = computerPlay(items);
 
 function playRound(playerSelection,computerSelection)
 {
 
 if (playerSelection == computerSelection)
-return "It's a tie!";
-else if(playerSelection == 'rock' & computerSelection == 'scissors')
-return "You win!";
-else if(playerSelection == 'paper' & computerSelection == 'rock')
-return "You win!";
-else if (playerSelection == 'scissors' & computerSelection == 'paper')
-return "You win!";
+return `It's a tie! You chose ${playerSelection} and the computer chose ${computerSelection}`;
+else if(playerSelection == `rock` & computerSelection == `scissors`)
+return `You win! ${playerSelection} beats ${computerSelection}!`;
+else if(playerSelection == `paper` & computerSelection == `rock`)
+return `You win! ${playerSelection} beats ${computerSelection}!`;
+else if (playerSelection == `scissors` & computerSelection == `paper`)
+return `You win! ${playerSelection} beats ${computerSelection}!`;
 else
-return "You Lose!";
+return `you Lose! ${computerSelection} beats ${playerSelection}!`;
 
 }
 
@@ -27,6 +27,6 @@ return items[Math.floor(Math.random()*items.length)];
 
 }
 
+playRound(playerSelection, computerSelection);
 
-
-console.log(computerSelection);
+console.log(`The computer chose ${computerSelection}`);
