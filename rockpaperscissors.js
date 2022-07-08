97cd79585f3feb30153a71b13f7b26211f0780ea
saxbonsai <jeon.niroumand@gmail.com>
2022-07-07 23:13:19 -0700

    working js in the consoe, need to create message for viewport.

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,2 +1,32 @@
-const input = prompt("Type: Rock, Paper or Scissors to play and press enter");
-console.log(`You chose ${input}`);
+const playerSelection = prompt("Type: Rock, Paper or Scissors to play and press enter");
+console.log(`You chose ${playerSelection.toLowerCase()}`);
+
+var items = ['rock','paper','scissors'];
+var computerSelection = computerPlay(items);
+
+function playRound(playerSelection,computerSelection)
+{
+
+if (playerSelection == computerSelection)
+return "It's a tie!";
+else if(playerSelection == 'rock' & computerSelection == 'scissors')
+return "You win!";
+else if(playerSelection == 'paper' & computerSelection == 'rock')
+return "You win!";
+else if (playerSelection == 'scissors' & computerSelection == 'paper')
+return "You win!";
+else
+return "You Lose!";
+
+}
+
+function computerPlay(items)  //computer picks rock,paper or scissors randomly
+{
+
+return items[Math.floor(Math.random()*items.length)];
+
+}
+
+
+
+console.log(computerSelection);
