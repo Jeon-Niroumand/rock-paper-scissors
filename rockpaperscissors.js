1a7776e846bd026fdd37d3c5afa76a35780f3219
Jeon Niroumand <jeon.niroumand@gmail.com>
2022-09-11 22:39:47 -0700

    Made line breaks in output hooked up display results

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,10 +1,8 @@
 var items = ['rock','paper','scissors'];
-const computerSelection = computerPlay(items);
+let computerSelection = computerPlay(items);
 
-function computerPlay(items)  //computer picks rock,paper or scissors randomly
-{
-
-return items[Math.floor(Math.random()*items.length)];
+function computerPlay(items) {  //computer picks rock, paper or scissors randomly
+    return items[Math.floor(Math.random()*items.length)];
 
 }
 
@@ -12,59 +10,63 @@ let win = 0, //initialize the variables used in playRound()
     lose = 0,
     draw = 0;
 
-function playRound(playerSelection,computerSelection) //win,lose,draw logic per round.
-{
-
-if (playerSelection == computerSelection)
-    return draw++,`It's a tie! You chose ${playerSelection} and the computer chose ${computerSelection}`;
-else if(playerSelection == `rock` & computerSelection == `scissors`)
-    return win++,`You win! ${playerSelection} beats ${computerSelection}!`;
-else if(playerSelection == `paper` & computerSelection == `rock`)
-    return win++,`You win! ${playerSelection} beats ${computerSelection}!`;
-else if (playerSelection == `scissors` & computerSelection == `paper`)
-    return win++,`You win! ${playerSelection} beats ${computerSelection}!`;
-else
-    return lose++,`you Lose! ${computerSelection} beats ${playerSelection}!`;
+function playRound(playerSelection,computerSelection) { //win, lose, draw logic per round.
+    if (playerSelection == computerSelection)
+        return draw++,`It's a tie!`;
+    else if(playerSelection == `rock` & computerSelection == `scissors`)
+        return win++,`You win! ${playerSelection} beats ${computerSelection}!`;
+    else if(playerSelection == `paper` & computerSelection == `rock`)
+        return win++,`You win! ${playerSelection} beats ${computerSelection}!`;
+    else if (playerSelection == `scissors` & computerSelection == `paper`)
+        return win++,`You win! ${playerSelection} beats ${computerSelection}!`;
+    else
+        return lose++,`You lose! ${computerSelection} beats ${playerSelection}!`;
 
 }
 
-function results() //game result logic, tallies win, lose, draw and prints winner(s)
-{
-
-if (win == lose)
-    return `It's a tie! You won ${win} rounds, lost ${lose} rounds, and tied ${draw} rounds`;
-else if (win > lose)
-    return `You win! You won ${win} rounds, lost ${lose} rounds, and tied ${draw} rounds`;
-else
-    return `You lose! You won ${win} round, lost ${lose} rounds, and tied ${draw} rounds`;
+function results() { //game result logic, tallies win, lose, draw and prints winner(s)
+    if (win == lose)
+        return `It's a tie! You won ${win} rounds, lost ${lose} rounds, and tied ${draw} rounds`;
+    else if (win > lose)
+        return `You win! You won ${win} rounds, lost ${lose} rounds, and tied ${draw} rounds`;
+    else
+        return `You lose! You won ${win} round, lost ${lose} rounds, and tied ${draw} rounds`;
 
 }
 
-function game() //plays 1 round of rock-paper-scissors
-{
+let round = 0;
+function game() { //plays 1 round of rock-paper-scissors
+    let rounds = 5; //change the number of rounds here
+    if (round < rounds) round++; 
+    else throw 'GAME OVER';
+    computerSelection = computerPlay(items);
+    resultWindow.innerHTML += `ROUND ${round} RESULTS:<br/>You chose ${playerSelection} and the computer chose ${computerSelection}<br/>${playRound(playerSelection, computerSelection)}<br/><br/>`;
+    if (round === rounds) return resultWindow.innerHTML += `***************FINAL RESULTS***************<br/>${results()}`;
+}
 
-const playerSelection = prompt("Type: rock, paper or scissors to play and press enter (best of 5 rounds wins)");
-console.log(`ROUND ${round} RESULTS:`)
-console.log(`You chose ${playerSelection.toLowerCase()}`);
-const computerSelection = computerPlay(items);
-console.log(`The computer chose ${computerSelection}`);
-console.log(playRound(playerSelection, computerSelection));
+const rock = document.getElementById("rock");
+rock.addEventListener("click", myRock);
+
+function myRock() {
+    playerSelection = "rock";
+    game()
 
 }
 
-/****UNCOMMENT WHEN DONE WITH UI**************let round = 1;
-for (round; round < 1; round++) //play 5 rounds and increment win.lose,draw vars.
-{
-game()
+const paper = document.getElementById("paper");
+paper.addEventListener("click", myPaper);
 
+function myPaper() {
+    playerSelection = "paper";
+    game()
+    
 }
 
-console.log(`***********************FINAL RESULTS***********************`)
-console.log(results());*/
+const scissors = document.getElementById("scissors");
+scissors.addEventListener("click", myScissors);
 
-const element = document.getElementById("rock");
-element.addEventListener("click", myFunction);
+function myScissors() {
+    playerSelection = "scissors";
+    game()
 
-function myFunction() {
-    alert("rock")
 }
